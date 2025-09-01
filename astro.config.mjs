ed85e76540e0a8189060d393669389696837f3b5
Christopher Lopes <350318+ciwchris@users.noreply.github.com>
2025-09-01 12:25:31 -0700

    Basic styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+
+const isProduction = process.env.NODE_ENV === 'production';
+const deployedDomain = 'https://ciwchris.github.io';
+const baseSubdirectory = '/short-thoughts/';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: isProduction ? deployedDomain : 'http://localhost:4321',
+  base: baseSubdirectory,
+  integrations: [tailwind({
+    config: {
+      applyBase: false,
+    }
+  })],
+});
---
+++ src/content/blog/first-post.md
@@ -0,0 +1,7 @@
+---
+title: 'My First Blog Post'
+pubDate: 2025-09-01
+description: 'This is the first post on my new blog.'
+---
+
+This is the content of my first blog post.
