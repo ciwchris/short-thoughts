35444b7b5ec7fab94c393c61f0b4b9f3acc5e8c8
Christopher Lopes <350318+ciwchris@users.noreply.github.com>
2025-10-04 13:42:57 -0700

    Limit number of posts on index and fix icons on smaller screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ export default {
         sans: ['Merriweather', ...defaultTheme.fontFamily.sans],
         mansalva: ['Mansalva', 'cursive'],
       },
+      screens: {
+        'xs': '480px',
+      },
     }
   },
   plugins: [
